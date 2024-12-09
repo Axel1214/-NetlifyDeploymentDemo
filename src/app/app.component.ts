8514ba1982fa683e4074371dda9371ac8eba49b0
SJH-706 <sjh-706@SJH-706-40.local>
2024-12-09 13:13:41 +0800

    No Bootstrap and Not Deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { DataBindingDemoComponent } from './data-binding-demo/data-binding-demo.component';
+import { RouterModule } from '@angular/router';  // Import RouterModule here
+import { DirectivesDemoComponent } from './directives-demo/directives-demo.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataBindingDemoComponent, DirectivesDemoComponent, RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
